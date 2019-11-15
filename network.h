8acaac701899622bcee058062675e7886f586cf7
claravalls <clara.valls@student.salle.url.edu>
2019-11-15 17:17:32 +0100

    network

---
+++ network.h
@@ -0,0 +1,10 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
